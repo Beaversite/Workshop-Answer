cb443e2e2436fe9359e5704ca8cbae53f48842fd
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2023-05-27 02:14:00 +0700

    ✨ : Day 2 Exercise 3

--- frontend/src/DisplayTodoItems.jsx
+++ frontend/src/DisplayTodoItems.jsx
@@ -1,23 +1,29 @@
-function DisplayTodoItems({ todoItem }) {
+function DisplayTodoItems({ todoItem, deleteTodoItem, checkTodoItem }) {
   if (todoItem.length === 0) {
-    return (
-      <div className="text-center text-gray-500">No todo found</div>
-    );
+    return <div className="text-center text-gray-500">No todo found</div>;
   }
 
   return (
     <>
       <div className="space-y-2">
-        {todoItem.map((todo, index) => (
+        {todoItem.map((todo) => (
           <div
-            key={index}
+            key={todo.id}
             className="flex justify-between items-center border shadow rounded p-2"
           >
             <div className="flex w-full gap-2">
-              <input type="checkbox" className="w-6 h-6" />
+              <input
+                type="checkbox"
+                className="w-6 h-6"
+                checked={todo.completed}
+                onChange={() => checkTodoItem(todo.id)}
+              />
               <span className="w-full">{todo.title}</span>
             </div>
-            <button className="p-2 rounded bg-red-500 hover:bg-red-400 active:bg-red-500 active:scale-95 text-white transition">
+            <button
+              onClick={() => deleteTodoItem(todo.id)}
+              className="p-2 rounded bg-red-500 hover:bg-red-400 active:bg-red-500 active:scale-95 text-white transition"
+            >
               Delete
             </button>
           </div>
--- frontend/src/TodoInfo.jsx
+++ frontend/src/TodoInfo.jsx
@@ -10,20 +10,73 @@ function TodoInfo() {
 
   const [todo, setTodo] = useState({}); // Todo ที่จะแสดง
   const [isEditing, setIsEditing] = useState(false); // กำลัง Edit อยู่หรือไม่
-  const todoItem = [];
+  const [title, setTitle] = useState(""); // ชื่อ Todo
+  const [todoItem, setTodoItem] = useState([]);
 
   useEffect(() => {
     const getTodo = async () => {
-      const fetchedTodo = await pb.collection("todo").getOne(id); // ทำการ Fetch Todo ที่มี ID ตามที่ระบุ
+      const fetchedTodo = await pb.collection("todo").getOne(id, {
+        expand: "todoItem",
+      }); // ทำการ Fetch Todo ที่มี ID ตามที่ระบุ
       setTodo(fetchedTodo);
+      setTodoItem(fetchedTodo.expand.todoItem || []);
     };
     getTodo();
   }, [id]);
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
 
   const updateTodo = async (todo) => {
     const updatedTodo = await pb.collection("todo").update(id, todo);
     setTodo(updatedTodo);
     setIsEditing(false);
+  };
+
+  const addTodoItem = async () => {
+    const newTodoItem = await pb.collection("todoItem").create({
+      title,
+    });
+    const updatedTodo = await pb.collection("todo").update(
+      id,
+      {
+        todoItem: [...todo.todoItem, newTodoItem.id],
+      },
+      {
+        expand: "todoItem",
+      }
+    );
+    setTodo(updatedTodo);
+    setTodoItem(updatedTodo.expand.todoItem);
+    setTitle("");
+  };
+
+  const deleteTodoItem = async (todoItemId) => {
+    const updatedTodo = await pb.collection("todo").update(
+      id,
+      {
+        todoItem: todo.todoItem.filter((todoItem) => todoItem !== todoItemId),
+      },
+      {
+        expand: "todoItem",
+      }
+    );
+    await pb.collection("todoItem").delete(todoItemId);
+    setTodo(updatedTodo);
+    setTodoItem(updatedTodo.expand.todoItem);
+  };
+
+  const checkTodoItem = async (todoItemId) => {
+    const todoItemInfo = todoItem.find((todoItem) => todoItem.id === todoItemId);
+    const updatedTodoItem = await pb.collection("todoItem").update(todoItemId, {
+      completed: !todoItemInfo.completed,
+    });
+    setTodoItem(
+      todoItem.map((todoItem) =>
+        todoItem.id === todoItemId ? updatedTodoItem : todoItem
+      )
+    );
   };
 
   return (
@@ -37,13 +90,25 @@ function TodoInfo() {
         )}
         <hr />
         <div className="flex w-ful gap-2">
-          <input type="text" className="border shadow rounded w-full p-1" />
-          <button className="p-2 rounded bg-green-500 hover:bg-green-400 active:bg-green-500 active:scale-95 text-white transition">
+          <input
+            onChange={handleTitleChange}
+            value={title}
+            type="text"
+            className="border shadow rounded w-full p-1"
+          />
+          <button
+            onClick={addTodoItem}
+            className="p-2 rounded bg-green-500 hover:bg-green-400 active:bg-green-500 active:scale-95 text-white transition"
+          >
             Add
           </button>
         </div>
         <hr />
-        <DisplayTodoItems todoItem={todoItem} />
+        <DisplayTodoItems
+          todoItem={todoItem}
+          deleteTodoItem={deleteTodoItem}
+          checkTodoItem={checkTodoItem}
+        />
       </div>
     </div>
   );
