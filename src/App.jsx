e519d3a57521c6428bde52eecadebacea980ec1e
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2023-05-26 10:51:45 +0700

    💄 : Day 1 Exercise 3

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function App() {
   return (
     <div className="w-full flex justify-center p-4">
       <div className="w-96 space-y-2">
-        <h1  className="text-3xl">Todo App</h1>
+        <h1 className="text-3xl">Todo App</h1>
         <label>
           Title
           <input
@@ -76,7 +76,10 @@ function App() {
           />
         </label>
         <div className="flex justify-end">
-          <button onClick={addTodo} className="p-2 rounded bg-green-500 hover:bg-green-400 active:bg-green-500 active:scale-95 text-white transition">
+          <button
+            onClick={addTodo}
+            className="p-2 rounded bg-green-500 hover:bg-green-400 active:bg-green-500 active:scale-95 text-white transition"
+          >
             Add
           </button>
         </div>
